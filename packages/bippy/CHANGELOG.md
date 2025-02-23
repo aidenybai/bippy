526de16521d8ba96170680961917afe62ac871c2
Aiden Bai <aiden.bai05@gmail.com>
2025-02-22 16:12:24 -0800

    chore: update bippy to version 0.3.7

--- packages/bippy/CHANGELOG.md
+++ packages/bippy/CHANGELOG.md
@@ -1,5 +1,11 @@
 # bippy
 
+## 0.3.7
+
+### Patch Changes
+
+- fix inspect mounting
+
 ## 0.3.6
 
 ### Patch Changes
--- packages/shrinkwrap/inject/puppeteer-utils.ts
+++
@@ -1,9 +0,0 @@
-import { extractColors } from 'extract-colors';
-
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-export const registerGlobal = (name: string, value: any) => {
-  // biome-ignore lint/suspicious/noExplicitAny: used by puppeteer
-  (globalThis as any)[name] = value;
-};
-
-registerGlobal('extractColors', extractColors);
