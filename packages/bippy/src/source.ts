2b953f129e33d936e473ef501ba7be4efcd56d68
Aiden Bai <aiden.bai05@gmail.com>
2025-05-01 22:54:41 -0700

    new source extraction method

--- packages/bippy/src/source.ts
+++ packages/bippy/src/source.ts
@@ -121,7 +121,7 @@ const parseStackFrame = async (frame: string): Promise<FiberSource | null> => {
   };
 };
 
-// https://github.com/hoxyq/react/blob/e450e6b97653fc5b7a56ec700e87546abfd91aa3/packages/react-devtools-shared/src/backend/DevToolsComponentStackFrame.js#L67
+// https://github.com/facebook/react/blob/f739642745577a8e4dcb9753836ac3589b9c590a/packages/react-devtools-shared/src/backend/shared/DevToolsComponentStackFrame.js#L22
 const describeNativeComponentFrame = (
   fn: React.ComponentType<unknown>,
   construct: boolean,
@@ -138,83 +138,114 @@ const describeNativeComponentFrame = (
   const previousDispatcher = currentDispatcherRef.current;
   currentDispatcherRef.current = null;
   const prevLogs = disableLogs();
-
-  const RunInRootFrame = {
-    DetermineComponentFrameRoot(): [string | null, string | null] {
-      let control: Error | undefined;
-      try {
-        if (construct) {
-          const Fake = () => {
-            throw Error();
-          };
-          Object.defineProperty(Fake.prototype, 'props', {
-            set: () => {
+  try {
+    /**
+     * Finding a common stack frame between sample and control errors can be
+     * tricky given the different types and levels of stack trace truncation from
+     * different JS VMs. So instead we'll attempt to control what that common
+     * frame should be through this object method:
+     * Having both the sample and control errors be in the function under the
+     * `DescribeNativeComponentFrameRoot` property, + setting the `name` and
+     * `displayName` properties of the function ensures that a stack
+     * frame exists that has the method name `DescribeNativeComponentFrameRoot` in
+     * it for both control and sample stacks.
+     */
+    const RunInRootFrame = {
+      DetermineComponentFrameRoot() {
+        // biome-ignore lint/suspicious/noExplicitAny: OK
+        let control: any;
+        try {
+          // This should throw.
+          if (construct) {
+            // Something should be setting the props in the constructor.
+            // biome-ignore lint/complexity/useArrowFunction: OK
+            const Fake = function () {
               throw Error();
-            },
-          });
-          if (typeof Reflect === 'object' && Reflect.construct) {
-            try {
-              Reflect.construct(Fake, []);
-            } catch (x) {
-              control = x as Error;
+            };
+            // $FlowFixMe[prop-missing]
+            Object.defineProperty(Fake.prototype, 'props', {
+              // biome-ignore lint/complexity/useArrowFunction: OK
+              set: function () {
+                // We use a throwing setter instead of frozen or non-writable props
+                // because that won't throw in a non-strict mode function.
+                throw Error();
+              },
+            });
+            if (typeof Reflect === 'object' && Reflect.construct) {
+              // We construct a different control for this case to include any extra
+              // frames added by the construct call.
+              try {
+                Reflect.construct(Fake, []);
+              } catch (x) {
+                control = x;
+              }
+              Reflect.construct(fn, [], Fake);
+            } else {
+              try {
+                // @ts-expect-error
+                Fake.call();
+              } catch (x) {
+                control = x;
+              }
+              // @ts-expect-error
+              fn.call(Fake.prototype);
             }
-            Reflect.construct(fn as new () => unknown, [], Fake);
           } else {
             try {
-              Fake.call(null);
+              throw Error();
             } catch (x) {
-              control = x as Error;
+              control = x;
             }
-            (fn as new () => unknown).call(Fake.prototype);
-          }
-        } else {
-          try {
-            throw Error();
-          } catch (x) {
-            control = x as Error;
-          }
-          const maybePromise = (fn as () => unknown)();
+            // TODO(luna): This will currently only throw if the function component
+            // tries to access React/ReactDOM/props. We should probably make this throw
+            // in simple components too
+            // @ts-expect-error
+            const maybePromise = fn();
 
-          if (
-            maybePromise &&
-            typeof maybePromise === 'object' &&
-            'catch' in maybePromise &&
-            typeof maybePromise.catch === 'function'
-          ) {
-            maybePromise.catch(() => {});
+            // If the function component returns a promise, it's likely an async
+            // component, which we don't yet support. Attach a noop catch handler to
+            // silence the error.
+            // TODO: Implement component stacks for async client components?
+            if (maybePromise && typeof maybePromise.catch === 'function') {
+              maybePromise.catch(() => {});
+            }
+          }
+          // biome-ignore lint/suspicious/noExplicitAny: OK
+        } catch (sample: any) {
+          // This is inlined manually because closure doesn't do it for us.
+          if (sample && control && typeof sample.stack === 'string') {
+            return [sample.stack, control.stack];
           }
         }
-      } catch (sample) {
-        if (
-          sample instanceof Error &&
-          control &&
-          control.stack &&
-          sample.stack
-        ) {
-          return [sample.stack, control.stack];
-        }
-      }
-      return [null, null];
-    },
-  };
+        return [null, null];
+      },
+    };
 
-  (
-    RunInRootFrame.DetermineComponentFrameRoot as React.ComponentType<unknown>
-  ).displayName = 'DetermineComponentFrameRoot';
-  const namePropDescriptor = Object.getOwnPropertyDescriptor(
-    RunInRootFrame.DetermineComponentFrameRoot,
-    'name',
-  );
-  if (namePropDescriptor?.configurable) {
-    Object.defineProperty(RunInRootFrame.DetermineComponentFrameRoot, 'name', {
-      value: 'DetermineComponentFrameRoot',
-    });
-  }
+    // @ts-expect-error
+    RunInRootFrame.DetermineComponentFrameRoot.displayName =
+      'DetermineComponentFrameRoot';
+    const namePropDescriptor = Object.getOwnPropertyDescriptor(
+      RunInRootFrame.DetermineComponentFrameRoot,
+      'name',
+    );
+    // Before ES6, the `name` property was not configurable.
+    if (namePropDescriptor?.configurable) {
+      // V8 utilizes a function's `name` property when generating a stack trace.
+      Object.defineProperty(
+        RunInRootFrame.DetermineComponentFrameRoot,
+        // Configurable properties can be updated even if its writable descriptor
+        // is set to `false`.
+        // $FlowFixMe[cannot-write]
+        'name',
+        { value: 'DetermineComponentFrameRoot' },
+      );
+    }
 
-  try {
     const [sampleStack, controlStack] =
       RunInRootFrame.DetermineComponentFrameRoot();
     if (sampleStack && controlStack) {
+      // This extracts the first frame from the sample that isn't also in the control.
+      // Skipping one frame that we assume is the frame that calls the two.
       const sampleLines = sampleStack.split('\n');
       const controlLines = controlStack.split('\n');
       let s = 0;
@@ -231,25 +262,49 @@ const describeNativeComponentFrame = (
       ) {
         c++;
       }
+      // We couldn't find our intentionally injected common root frame, attempt
+      // to find another common root frame by search from the bottom of the
+      // control stack...
       if (s === sampleLines.length || c === controlLines.length) {
         s = sampleLines.length - 1;
         c = controlLines.length - 1;
         while (s >= 1 && c >= 0 && sampleLines[s] !== controlLines[c]) {
+          // We expect at least one stack frame to be shared.
+          // Typically this will be the root most one. However, stack frames may be
+          // cut off due to maximum stack limits. In this case, one maybe cut off
+          // earlier than the other. We assume that the sample is longer or the same
+          // and there for cut off earlier. So we should find the root most frame in
+          // the sample somewhere in the control.
           c--;
         }
       }
       for (; s >= 1 && c >= 0; s--, c--) {
+        // Next we find the first one that isn't the same which should be the
+        // frame that called our sample function and the control.
         if (sampleLines[s] !== controlLines[c]) {
+          // In V8, the first line is describing the message but other VMs don't.
+          // If we're about to return the first line, and the control is also on the same
+          // line, that's a pretty good indicator that our sample threw at same line as
+          // the control. I.e. before we entered the sample frame. So we ignore this result.
+          // This can happen if you passed a class to function component, or non-function.
           if (s !== 1 || c !== 1) {
             do {
               s--;
               c--;
+              // We may still have similar intermediate frames from the construct call.
+              // The next one that isn't the same should be our match though.
               if (c < 0 || sampleLines[s] !== controlLines[c]) {
+                // V8 adds a "new" prefix for native classes. Let's remove it to make it prettier.
                 let frame = `\n${sampleLines[s].replace(' at new ', ' at ')}`;
+
                 const displayName = getDisplayName(fn);
+                // If our component frame is labeled "<anonymous>"
+                // but we have a user-provided "displayName"
+                // splice it in to make the stack more readable.
                 if (displayName && frame.includes('<anonymous>')) {
                   frame = frame.replace('<anonymous>', displayName);
                 }
+                // Return the line we found.
                 return frame;
               }
             } while (s >= 1 && c >= 0);
@@ -260,8 +315,11 @@ const describeNativeComponentFrame = (
     }
   } finally {
     reentry = false;
+
     Error.prepareStackTrace = previousPrepareStackTrace;
-    currentDispatcherRef.current = previousDispatcher;
+
+    // biome-ignore lint/suspicious/noExplicitAny: OK
+    (currentDispatcherRef as any).H = previousDispatcher;
     reenableLogs(prevLogs);
   }
 
@@ -323,7 +381,7 @@ export const getFiberSource = async (
             if (isCompositeFiber(f)) return true;
           },
           true,
-        ),
+        )?.type,
       )
     : getType(fiber.type);
   if (!componentFunction || reentry) {
