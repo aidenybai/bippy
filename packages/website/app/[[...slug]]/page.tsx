775b69e8eee34ee25f6fcd5ea356c72d153e0227
Aiden Bai <aiden.bai05@gmail.com>
2026-09-05 00:45:03 -0700

    fix: redirect bippy.dev to the GitHub README (#110)

--- packages/website/app/[[...slug]]/page.tsx
+++ packages/website/app/[[...slug]]/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
-const CatchAll = () => redirect("https://aidenybai.com/bippy");
+const CatchAll = () => redirect("https://github.com/aidenybai/bippy#readme");
 
 export default CatchAll;
